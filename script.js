4f42785d79a16ec7b791d18171a8f35c4dac0749
Durgesh Chirmade <89195894+DurgeshChirmade@users.noreply.github.com>
2021-08-30 11:21:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+ window.onload = function(){
+     window.addEventListener("scroll", function(){
+         let navbar  = document.getElementById("menu-bar");
+
+         if(window.pageYOffset >= 190){
+             navbar.classList.add("sticky");
+         }
+         else{
+             navbar.classList.remove("sticky");
+         }
+     });
+ }
